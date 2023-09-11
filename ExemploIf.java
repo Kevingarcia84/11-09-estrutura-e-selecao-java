892330a9b885f4098a0707b3bdcb4ca10a748edc
Kevin Garcia <kevin84rg@gmail.com>
2023-09-11 20:53:42 -0300

    ap03 (selecao) : import e estrutura

---
+++ ExemploIf.java
@@ -0,0 +1,13 @@
+//java.lan é o pacote do Java
+import javax.swing.JOptionPane;
+public class ExemploIf {
+    static public void main (String [] args){
+        //declaração de variáveis
+        
+        //entrada
+
+        //processamento
+
+        //saída
+    }
+}
